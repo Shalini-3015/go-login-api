1ac981e860e0c26e2c5831b3a16e0fd5e68be960
Shalini-3015 <shalini.joshi@ima60.com>
2026-02-03 08:15:42 +0000

    feature(1-7): configure auth routes

---
+++ router/router.go
@@ -0,0 +1,18 @@
+package router
+
+import (
+	"go-login-api-task/controller"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRouter() *gin.Engine {
+	r := gin.Default()
+
+	authController := controller.NewAuthController()
+
+	// Auth routes
+	r.POST("/login", authController.Login)
+
+	return r
+}
