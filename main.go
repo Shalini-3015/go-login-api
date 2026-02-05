03b9e8a6bae38a60cf0c85569e07fca3199ab6ca
Shalini-3015 <shalini.joshi@ima60.com>
2026-02-05 18:06:58 +0000

    chore(1-30):wire currency, exchange rate and conversion routes with auth

--- main.go
+++ main.go
@@ -1,23 +1,25 @@
 package main
 
 import (
+	"go-login-api-task/config"
+	"go-login-api-task/models"
+	"go-login-api-task/router"
 
-"go-login-api-task/config"
-"go-login-api-task/models"
-"go-login-api-task/router"
-
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	r := gin.Default()
 	config.ConnectDatabase()
 	migrateModels()
-	r := router.SetupRouter()
+	router.SetupRouter(r)
 	r.Run(":8080")
 }
 
 func migrateModels() {
 	config.DB.AutoMigrate(
 		&models.LoginUser{},
-		
+		&models.Currency{},
+		&models.ExchangeRate{},
 	)
 }
