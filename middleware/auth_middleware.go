4c0168b2801fcad2bd60c55e8a2085b6ce9756cc
Shalini-3015 <shalini.joshi@ima60.com>
2026-02-05 18:05:56 +0000

    feat(1-29):add jwt auth for protected APIs

---
+++ middleware/auth_middleware.go
@@ -0,0 +1,53 @@
+package middleware
+
+import (
+	"net/http"
+	"strings"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var jwtSecret = []byte("secret_key") 
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+		authHeader := ctx.GetHeader("Authorization")
+		if authHeader == "" {
+			ctx.JSON(http.StatusUnauthorized, gin.H{"error": "authorization header missing"})
+			ctx.Abort()
+			return
+		}
+
+		parts := strings.Split(authHeader, " ")
+		if len(parts) != 2 || parts[0] != "Bearer" {
+			ctx.JSON(http.StatusUnauthorized, gin.H{"error": "invalid authorization format"})
+			ctx.Abort()
+			return
+		}
+
+		tokenStr := parts[1]
+		fmt.Println("TOKEN:", tokenStr)
+		
+
+
+
+		token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+			return jwtSecret, nil
+		})
+
+		if err != nil || !token.Valid {
+			ctx.JSON(http.StatusUnauthorized, gin.H{"error": "invalid or expired token"})
+			ctx.Abort()
+			return
+		}
+
+		// optional: store claims in context
+		if claims, ok := token.Claims.(jwt.MapClaims); ok {
+			ctx.Set("user_id", claims["user_id"])
+		}
+
+		ctx.Next()
+	}
+}
