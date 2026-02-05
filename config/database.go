3d9a484112c77521d22d363df0c0ac013070e972
Shalini-3015 <shalini.joshi@ima60.com>
2026-02-05 17:43:29 +0000

    chore(1-18):configure postgres connection

--- config/database.go
+++ config/database.go
@@ -1,22 +1,41 @@
 package config
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	dsn := "host=localhost user=go_user password=go_pass dbname=go_login port=5432 sslmode=disable"
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=UTC",
+		getEnv("DB_HOST", "localhost"),
+		getEnv("DB_USER", "go_user"),
+		getEnv("DB_PASSWORD", "go_pass"),
+		getEnv("DB_NAME", "go_login"),
+		getEnv("DB_PORT", "5432"),
+	)
 
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
 	if err != nil {
 		log.Fatal("Failed to connect to database:", err)
 	}
 
 	DB = db
-	log.Println("Database connected successfully")
+	log.Println("Database connection established")
+}
+
+func getEnv(key, defaultVal string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	return defaultVal
 }
